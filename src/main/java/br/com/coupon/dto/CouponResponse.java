1c9b150fdbc63d42ef5a0bb02ad6223b802b59d4
Diego Macario <diego_macario@hotmail.com>
2026-05-31 19:19:05 -0300

    feat: criando o objeto que representa a response

---
+++ src/main/java/br/com/coupon/dto/CouponResponse.java
@@ -0,0 +1,19 @@
+package br.com.coupon.dto;
+
+import br.com.coupon.domain.CouponStatus;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record CouponResponse(
+        UUID id,
+        String code,
+        String description,
+        BigDecimal discountValue,
+        LocalDateTime expirationDate,
+        CouponStatus status,
+        boolean published,
+        boolean redeemed
+) {
+}
